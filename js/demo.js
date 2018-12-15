68c9ce09c5634e2c850eec2003c4f8c84ee8d531
Han09 <39734858+Han09@users.noreply.github.com>
2018-12-15 15:06:21 +0800

    修改小屏幕时的导航栏样式，添加古诗词API

--- js/demo.js
+++ js/demo.js
@@ -1,45 +1,23 @@
-$(window).resize(function () {
-    media();
-})
-$(window).scroll(function () {
-    media();
-})
-
 //导航条样式变化
 function navbar() {
     var top = $("html,body").scrollTop();
     if (top > 10) {
         $(".navbar").addClass("navbar-gd", 2000)
         $(".navbar-default .navbar-nav > li > a ").addClass("navbardea-gd", 2000)
+        $(".navbar-toggle").addClass()
     }
     else {
         $(".navbar").removeClass("navbar-gd", 2000)
         $(".navbar-default .navbar-nav > li > a ").removeClass("navbardea-gd", 2000)
     }
 }
-
-//媒体查询
-function media() {
-    var result1 = window.matchMedia('(min-width:1200px)');
-    var result2 = window.matchMedia('(min-width:992px)');
-    var result3 = window.matchMedia('(min-width:768px)');
-    if (result1.matches) {
-        console.log("大屏幕(>=1200)");//判断窗口状态，，会大量输出console，后期删除
-        navbar();
-    } else if (result2.matches) {
-        console.log("中等屏幕(>=992&<=1200)");//判断窗口状态，，会大量输出console，后期删除
-        navbar();
-    } else if (result3.matches) {
-        console.log("小屏幕(>=768&<=992)");//判断窗口状态，，会大量输出console，后期删除
-        navbar();
-    } else {
-        console.log("超小屏幕(<=768)");//判断窗口状态，，会大量输出console，后期删除
-    }
-}
-
 //滚动箭头平滑效果
 $(document).ready(function () {
-    $(".scroll-down").click(function () {
+    $(window).scroll(function () {
+        navbar();
+    })
+
+    $(".scroll-down").click(function  () {
         var target_top = $("#section1").offset().top;
         $("html,body").animate({scrollTop: target_top}, 1000);//跳转动画
         $("html,body").scrollTop(target_top);
