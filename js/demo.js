85e5dc79acd1debf6d9dcbad6ab177555075c283
Han09 <39734858+Han09@users.noreply.github.com>
2018-12-18 22:26:10 +0800

    补全部分内容

--- js/demo.js
+++ js/demo.js
@@ -1,7 +1,7 @@
 //导航条样式变化
 function navbar() {
     var top = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop
-  //  console.log(top);
+    //  console.log(top);
     if (top > 10) {
         $(".navbar").addClass("navbar-gd", 2000)
         $(".navbar-default .navbar-nav > li > a ").addClass("navbardea-gd", 2000)
@@ -15,40 +15,38 @@ function navbar() {
     }
 }
 
-function mmve(){
-    $('.jumbotron').mousemove(function(e){
+function mmve() {
+    $('.jumbotron').mousemove(function (e) {
         //大屏幕时的巨幕移动效果
-        var x = (e.pageX*-1/20);
+        var x = (e.pageX * -1 / 20);
         //  console.log("x="+x);
-        $(this).css("background-position-x",x+"px");
+        $(this).css("background-position-x", x + "px");800
     });
 }
 
 
 function media() {
-   // console.log("media执行");
+    // console.log("media执行");
     var result1 = window.matchMedia('(min-width:1200px)');
     var result2 = window.matchMedia('(min-width:992px)');
     var result3 = window.matchMedia('(min-width:768px)');
-    if(result1.matches) {
-       //console.log("大屏幕(>=1200)");
+    if (result1.matches) {
+        //console.log("大屏幕(>=1200)");
         $(".scroll-down").show();
     }
-    else if(result2.matches){
-       // console.log("中等屏幕(>=992&<=1200)");
+    else if (result2.matches) {
+        // console.log("中等屏幕(>=992&<=1200)");
         $(".scroll-down").show();
     }
-    else if(result3.matches){
-       //console.log("小屏幕(>=768&<=992)");
+    else if (result3.matches) {
+        //console.log("小屏幕(>=768&<=992)");
         $(".scroll-down").hide();
     }
-    else{
-       // console.log("超小屏幕(<=768)");
+    else {
+        // console.log("超小屏幕(<=768)");
         $(".scroll-down").hide();
     }
 }
-
-
 
 
 //滚动箭头平滑效果
@@ -56,7 +54,7 @@ $(document).ready(function () {
     $(window).scroll(function () {//滚动监听
         navbar();
     });
-    $(".scroll-down").click(function  () { //点击箭头时执行
+    $(".scroll-down").click(function () { //点击箭头时执行
         var target_top = $("#section1").offset().top;
         $("html,body").animate({scrollTop: target_top}, 1000);//跳转动画
         $("html,body").scrollTop(target_top);
@@ -66,7 +64,7 @@ $(document).ready(function () {
     mmve();
 
 });
-$(window).resize(function() { //窗口大小变化时执行
+$(window).resize(function () { //窗口大小变化时执行
     media();
 });
 
