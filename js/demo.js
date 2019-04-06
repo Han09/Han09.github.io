6c6def551ae9a845283af98ffd90a3b9dbcf91a4
韩林轩 <qdhanlinxuan@gmail.com>
2019-04-06 14:01:55 +0800

    内容界面重做

--- js/demo.js
+++ js/demo.js
@@ -14,26 +14,29 @@ function navbar() {
         $(".navbar-toggle").addClass();
         $(".navbar-header").addClass();
 
-    }
-    else {
+    } else {
         $(".navbar").removeClass("navbar-gd", 2000)
         $(".navbar-default .navbar-nav > li > a ").removeClass("navbardea-gd", 2000)
     }
 }
 
 function mmve() {
-    $('.jumbotron').mousemove(function (e) {
-        //大屏幕时的巨幕移动效果
-        var x = (e.pageX * -1 / 20);
-        //  console.log("x="+x);
-        $(this).css("background-position-x", x + "px");
-        800
-    });
+    if (window.innerWidth >= 1024) {
+        $('.jumbotron').mousemove(function (e) {
+            //大屏幕时的巨幕移动效果
+            var x = (e.pageX * -1 / 20);
+            //  console.log("x="+x);
+            $(this).css("background-position-x", x + "px");
+            800
+        });
+    }
 }
 //滚动箭头平滑效果
 $(document).ready(function () {
+
     $(window).scroll(function () {//滚动监听
         navbar();
+        hgroupfunction();
     });
     $(".scroll-down").click(function () { //点击箭头时执行
         var target_top = $("#section1").offset().top;
@@ -43,6 +46,22 @@ $(document).ready(function () {
     navbar();
     mmve();
 
+
+
 });
 
 
+//文本动态显示
+var hgroups = $('.items>hgroup')
+var benchmark = window.innerHeight/1.8;
+function hgroupfunction (){
+    // console.log(benchmark);
+    for(i=0;i<hgroups.length;i++){
+        let x = hgroups[i].getBoundingClientRect().y;
+        if (x<benchmark){
+            hgroups[i].classList.add('show')
+        }
+    }
+}
+//文本动态显示
+// var c = $('.item>hgroup').getBoundingClientRect().x;
